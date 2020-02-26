0acecd78ccf9fc30e2b510efe2529c08ab1e42da
Peter Haagsma <pjh26@students.calvin.edu>
2020-02-26 14:35:11 -0500

    Adding temp information for bolt model training

---
+++ SerialComm.py
@@ -0,0 +1,4 @@
+	python3  /home/SeniorDesign/create_tfrecord.py 		\
+			--data_dir=/home/data --set=train			\
+			--output_path=/home/data/train.record		\
+			--label_map_path=/home/data/label_map.pbtxt
